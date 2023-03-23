21500077631720d9b3f27a44fc53ca66ad3accfd
Akash-Kumbar <akashask50@gmail.com>
2023-03-23 16:41:52 +0530

    Made changes to main, now it is ready for remote

--- main.cpp
+++ main.cpp
@@ -6,5 +6,7 @@ int main()
     std::cout << "Learning git" << std::endl;
     std::cout << "Another rubbish line" << std::endl;
     std::cout << "Created a new branch, lets see how things work" << std::endl;
+    std::cout << "In a new branch, pushing it to the remote repo" << std::endl;
+
     return 0;
 }
