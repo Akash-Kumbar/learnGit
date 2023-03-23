62ab06942c4ca99cbf259bbe9c9981c62d3e263b
Akash-Kumbar <akashask50@gmail.com>
2023-03-23 16:28:50 +0530

    Trying to create a conflict

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
+    std::cout << "Making changes to create conflicts" << std::endl;
     std::cout << "Hello world" << std::endl;
     std::cout << "Learning git" << std::endl;
     std::cout << "Another rubbish line" << std::endl;
