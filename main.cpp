b80c6f281579eacfdba32ba2278cc6be1033997c
Akash-Kumbar <akashask50@gmail.com>
2023-03-23 15:20:03 +0530

    First commit to new branch

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main()
     std::cout << "Hello world" << std::endl;
     std::cout << "Learning git" << std::endl;
     std::cout << "Another rubbish line" << std::endl;
+    std::cout << "Created a new branch, lets see how things work" << std::endl;
     return 0;
 }
