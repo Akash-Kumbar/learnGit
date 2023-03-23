95f24cd842505918703221ff5e126152bb6ccc49
Akash-Kumbar <akashask50@gmail.com>
2023-03-23 16:50:59 +0530

    Final commit

--- main.cpp
+++ main.cpp
@@ -9,13 +9,10 @@ int main()
     std::cout << "Making changes in the remote repo to cause push conflict" << std::endl;
 
     std::cout << "Another rubbish line" << std::endl;
-<<<<<<< HEAD
     std::cout << "Created a new branch, lets see how things work" << std::endl;
     std::cout << "In a new branch, pushing it to the remote repo" << std::endl;
-=======
 
     std::cout << "Making changes in the local repo" << std::endl;
->>>>>>> master
 
     return 0;
 }
