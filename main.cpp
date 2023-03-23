d46bb28d67683b8d79b205d7cebafdcfa04524fe
Akash-Kumbar <akashask50@gmail.com>
2023-03-23 16:33:59 +0530

    Made changes to create conflicts

--- main.cpp
+++ main.cpp
@@ -3,10 +3,12 @@
 int main()
 {
     std::cout << "Making changes to create conflicts" << std::endl;
-    std::cout << "Hello world" << std::endl;
+   
     std::cout << "Learning git" << std::endl;
+
+
     std::cout << "Another rubbish line" << std::endl;
-    std::cout << "Made changes in the remote repo" << std::endl;
+
     std::cout << "Making changes in the local repo" << std::endl;
 
     return 0;
